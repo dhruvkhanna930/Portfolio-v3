46a4f8f9fa87121071294911d2923cf291ab346f
Dhruv Khanna <dhruvkhanna930@gmail.com>
2025-02-09 17:32:08 +0530

    Deployment: Issues fixed for react-dom/server.browser

--- components/magicui/icon-cloud.tsx
+++ components/magicui/icon-cloud.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import { renderToString } from "react-dom/server";
+import { renderToString } from "react-dom/server.browser";
 
 interface Icon {
   x: number;
